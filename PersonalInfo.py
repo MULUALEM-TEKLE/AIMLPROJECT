a9571e92d97a73285072296b9b47a45e212fbbdf
MULUALEM-TEKLE <you@example.com>
2019-03-07 14:15:22 +0300

    The Personal Information Class is added

---
+++ PersonalInfo.py
@@ -0,0 +1,35 @@
+
+def class PersonalInformation:
+    def __init__(self):
+        # essential informations
+        first_name , last_name
+        email
+        age
+        sex
+        pass
+    
+    def get_first_name(self):
+        pass
+    def set_first_name(self , first_name):
+        pass
+
+    def get_last_name(self):
+        pass
+    def set_last_name(self , last_name):
+        pass
+
+    def get_age(self):
+        pass
+    def set_age(self , age):
+        pass
+
+    def get_email(self):
+        pass
+    def set_email(self , email):
+        pass
+
+
+    def get_sex(self):
+        pass
+    def set_sex(self , sex):
+        pass
