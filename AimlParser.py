0b81892d2eea67172e16227e841b697326fd990c
MULUALEM-TEKLE <you@example.com>
2019-03-07 11:57:36 +0300

    The basic Structure of the AimlParser class is defined

---
+++ AimlParser.py
@@ -0,0 +1,17 @@
+# This class handles everything that is related with aiml proccessing
+
+class AimlParser:
+    def __init__(self , aiml_xml , user):
+        # this will set the user object ; and this will set the aiml_xml to it self
+        pass
+
+    def get_response(self , message):
+        # this will also add the question object to the user object
+        # returns an answer for the message from the aiml_xml
+        pass
+
+    def update_user_object(self):
+        # this will updte the user object on the fly
+        pass
+    
+    
