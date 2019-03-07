449de5111d3a7ca367b41d56be71fba62b11fbbb
MULUALEM-TEKLE <you@example.com>
2019-03-07 14:01:00 +0300

    Question class structure is defined

--- AimlParser.py
+++ AimlParser.py
@@ -15,3 +15,5 @@ class AimlParser:
         pass
     
     
+
+
---
+++ Question.py
@@ -0,0 +1,22 @@
+
+class Question:
+    def __init__(self , user , question_str):
+        self.user = user 
+        self.question_str = question_str
+        self.accuracy = 0.0
+        self.related_nodes = []
+        pass
+    
+    def answer(self):
+        # returns the answer
+        pass
+
+    # this functionality will be done later dont bother about it
+    def add_related_qn_node(self , qn):
+        if add_related_qn_node.length <=4:
+            self.related_nodes.add(qn)
+        else:
+            # cannot preceed
+        pass
+
+    
