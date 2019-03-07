a474d1dc713e33037e0fc56a29c45794d1e41715
MULUALEM-TEKLE <you@example.com>
2019-03-07 14:45:57 +0300

    GenerateQuestion class is defined

---
+++ GenerateQuestion.py
@@ -0,0 +1,11 @@
+
+class GenerateQuestion:
+    def __init__(self , xml):
+        pass
+
+    def make_qn(self):
+        # constructs a question object
+
+    def get_question(self):
+        # returns the qn object synthesized by this class
+        pass
