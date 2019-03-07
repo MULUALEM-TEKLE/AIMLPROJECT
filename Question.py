95f84c0a3977c07ed2e009bbbfbf88068a4f1ed5
MULUALEM-TEKLE <you@example.com>
2019-03-07 20:57:09 +0300

    The kernel object is injected to the question constructor to enable the answering of the question

--- Question.py
+++ Question.py
@@ -1,28 +1,32 @@
 
 class Question:
-    def __init__(self , user , question_str):
+    def __init__(self , user , question_str , kernel):
         self.user = user 
         self.question_str = question_str
         self.accuracy = 0.0
         self.related_nodes = []
+        self.kernel = kernel
         pass
     
     def answer(self):
         # persist the qn 
-        user.add_qn(self)
+        # self.user.add_qn(self)
+        response = self.kernel.respond(self.question_str)
         # returns the answer
-        pass
+        return response
+        
 
     # this functionality will be done later dont bother about it
-    def add_related_qn_node(self , qn):
-        if add_related_qn_node.length <=4:
-            self.related_nodes.add(qn)
-        else:
-            # cannot preceed
-        pass
+    # def add_related_qn_node(self , qn):
+    #     if add_related_qn_node.length <=4:
+    #         self.related_nodes.add(qn)
+    #     else:
+    #         return
+    #         # cannot preceed
+    #         pass
 
     # this function is used for node navigation
-    def move(self , index):
-        qn = self.related_nodes[index]
-        qn.add(self)
-        return qn
+    # def move(self , index):
+    #     qn = self.related_nodes[index]
+    #     qn.add(self)
+    #     return qn
