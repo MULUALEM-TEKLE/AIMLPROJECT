b180ad4144a3486d3116b7586cfe3a4b24af9c08
MULUALEM-TEKLE <you@example.com>
2019-03-07 14:29:15 +0300

    Driver (main) class is added

---
+++ DRIVER.py
@@ -0,0 +1,6 @@
+def main():
+    # Takes the user SID
+    # creates the user object 
+    # while true
+        # takes the qn of the uesr and create qn object and injects it with user obj 
+        # returns the answer
