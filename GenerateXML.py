db3c1f88ad97ad54406a936051e4d3fa4a3ca886
MULUALEM-TEKLE <you@example.com>
2019-03-07 14:59:36 +0300

    GenerateXML class is defined

---
+++ GenerateXML.py
@@ -0,0 +1,12 @@
+
+class GenerateXML:
+    def __init__(self , qn):
+        pass
+
+    def make_xml(self):
+        # constructs a question object
+
+    def get_xml(self):
+        # returns the qn object synthesized by this class
+        pass
+
