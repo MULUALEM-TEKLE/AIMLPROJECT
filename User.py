d76d32fcf9575c6729eda0e0401f18a5ef60313b
MohammedAlewi <rofyalewi@gmail.com>
2019-03-08 04:38:08 +1400

    update on user class

--- User.py
+++ User.py
@@ -7,5 +7,8 @@ class User:
     def add_qn(self , qn):
         # persist the question to the database
         pass
+    def get_personal_info(self):
+        # get the question to the database
+        pass
 
     
