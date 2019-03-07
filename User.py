312cef50660b064c94c86f3ac7e9cc14472faf61
MULUALEM-TEKLE <you@example.com>
2019-03-07 14:38:23 +0300

    User class is defined

--- User.py
+++ User.py
@@ -1,4 +1,11 @@
 
 class User:
-    def __init__(self):
+    def __init__(self , SID , personal_info):
+        self.list_of_qns = []
         pass
+
+    def add_qn(self , qn):
+        # persist the question to the database
+        pass
+
+    
