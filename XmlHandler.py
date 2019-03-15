df5fb2391dc30c510d3c177d8badca1c7b994d9b
Nahu Haile <haile.nahu18@gmail.com>
2019-03-15 01:38:25 -0700

    modified xml handler functions

--- XmlHandler.py
+++ XmlHandler.py
@@ -29,17 +29,70 @@ def viewTag(xmlObj):
     
 
 
-#viewTag(xmlObj)
-def writeXml(contentPat, contentLrn):
+#write a simple category including 'learn' tag in file
+def writeCategoryLrn(contentPat, contentLrn):
     new_category = et.SubElement(root,"category")
     new_pattern = et.SubElement(new_category,"pattern")
     new_template = et.SubElement(new_category,"template")
-#     new_learn = et.SubElement(new_template,"learn")
+    new_learn = et.SubElement(new_template,"learn")
 
-    new_pattern.text = contentPat
-    new_template.text = contentLrn
+    new_pattern.text = contentPat.upper()
+    new_learn.text = contentLrn
 
     xmlObj.write('reed.xml')
 
+#write a simple category in file
+def writeCategory(contentPat,contentTemp):
+    new_category = et.SubElement(root,"category")
+    new_pattern = et.SubElement(new_category,"pattern")
+    new_template = et.SubElement(new_category,"template")
 
-writeXml("Hi how are you", "I am fine thank you")
+    new_pattern.text = contentPat.upper()
+    new_template.text = contentTemp
+
+    xmlObj.write('reed.xml')
+
+    
+#wrire a catagory with 'srai' tag
+def writeCategorySrai(contentPat, contentSrai):
+    new_category = et.SubElement(root,"category")
+    new_pattern = et.SubElement(new_category,"pattern")
+    new_template = et.SubElement(new_category,"template")
+    new_srai = et.SubElement(new_template,"srai")
+
+    new_pattern.text = contentPat.upper()
+    new_srai.text = contentSrai
+    xmlObj.write('reed.xml')
+
+#write a category with 'random' and 'li'
+def writeCategoryRand(contentPat, *contentLi):
+    new_category = et.SubElement(root,"category")
+    new_pattern = et.SubElement(new_category,"pattern")
+    new_template = et.SubElement(new_category,"template")
+    new_random = et.SubElement(new_template,"random")
+    for li in contentLi:
+        new_li = et.SubElement(new_random,"li")
+        new_li.text = li
+
+    new_pattern.text = contentPat.upper()
+    xmlObj.write('reed.xml')
+
+
+#write a category with 'that' tag
+def writeCategoryThat(contentPat, contentThat,contentTemp):
+    new_category = et.SubElement(root,"category")
+    new_pattern = et.SubElement(new_category,"pattern")
+    new_that = et.SubElement(new_category,"that")
+    new_template = et.SubElement(new_category,"template")
+
+    new_pattern.text = contentPat.upper()
+    new_that.text = contentThat
+    new_template.text = contentTemp
+    xmlObj.write('reed.xml')
+writeCategoryThat("pattern *", "thhhaat", "hi")
+
+#write a new topic
+def writeTopic
+'''for movie in root.iter('learn'):
+    print(movie.attrib)
+'''
